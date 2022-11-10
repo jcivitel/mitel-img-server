12114222d11266cb9d1bc4c537038ce69cae8f3e
Jan Civitelli <jan.civitelli@dolphin-it.de>
2022-11-10 11:49:31 +0100

    staging: added generator app

---
+++ mitel_img_generator/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MitelImgGeneratorConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'mitel_img_generator'
--- mitel_img_server/settings.py
+++ mitel_img_server/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ujubm!m8$_f4axy(^g0v^!xd$ck!q7x#(-97_dc*m(a(r6bn!*'
+SECRET_KEY = config("DJANGO_SECRET_KEY", default='django-insecure-ujubm!m8$_f4axy(^g0v^!xd$ck!q7x#(-97_dc*m(a(r6bn!*', cast=str)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
