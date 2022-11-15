fa9be62907282998457fa219d3951127d03f4ef5
Jan Civitelli <jan.civitelli@dolphin-it.de>
2022-11-15 14:58:59 +0100

    fixed dynamic logo size

--- mitel_img_generator/views.py
+++ mitel_img_generator/views.py
@@ -11,7 +11,7 @@ def get_image(request, customer, phonemodel):
     my_phonemodel = PhoneModel.objects.get(phone_model=phonemodel)
     renderd_img = Image.new(mode="RGBA", size=(my_phonemodel.size_x, my_phonemodel.size_y), color=(255, 255, 255))
     cus_img = Image.open(my_customer.customer_logo)
-    cus_img = cus_img.resize((70, 50))
+    cus_img = cus_img.resize((int(my_phonemodel.size_x/5), int(my_phonemodel.size_y/5)))
     renderd_img.paste(cus_img, (my_phonemodel.pos_x, my_phonemodel.pos_y))
     output = io.BytesIO()
     renderd_img.save(output, format='PNG')
